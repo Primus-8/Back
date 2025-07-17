2aaeb59b55d301f7f9074b5b31497e5c14f9d23a
Sudama <sy7884212@gmail.com>
2025-07-17 23:54:08 +0530

    Update route.js

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,3 +1,20 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
+
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { uploadthingMiddleware } from "uploadthing/next/middleware";
 
@@ -14,3 +31,17 @@ export type OurFileRouter = typeof ourFileRouter;
 export const { POST } = uploadthingMiddleware({
   router: ourFileRouter,
 });
+
+export const POST = uploadthingMiddleware({
+  router: ourFileRouter,
+});
+
+// CORS header add karo POST response me bhi:
+export const POST = async (req) => {
+  const res = await uploadthingMiddleware({
+    router: ourFileRouter,
+  }).POST(req);
+
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  return res;
+};
